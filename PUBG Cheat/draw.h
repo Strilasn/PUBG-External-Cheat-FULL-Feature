8c7f9ba1e003495cccd5d27c79e59cc40919f3dc
Easton13243 <sd98f7234rhfgd@outlook.com>
2024-09-29 01:02:18 +0400

    Upload Files

---
+++ PUBG Cheat/draw.h
@@ -0,0 +1,43 @@
+// include the basic windows header files and the Direct3D header file
+/*#include <windows.h>
+#include <windowsx.h>
+#include <d3d9.h>
+
+#include <Dwmapi.h> 
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d9.lib")
+#pragma comment(lib,"Dwmapi.lib")
+
+// global declarations
+LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+HWND hWnd;
+HWND gWnd;
+ID3DXLine* d3dLine;
+LPD3DXFONT pFont;
+RECT rc2;
+
+#define PI 3.14159265
+
+static D3DPRESENT_PARAMETERS    g_d3dpp = {};
+
+int Width = 1920;
+int Height = 1080;
+int FOV;
+int SpectateCount = 69;
+
+LPCSTR gOverlay = "PLAYERUNKNOWN'S BATTLEGROUNDS ";
+
+int overlayPosX = 0;
+int overlayPosY = 0;
+int overlayWidth = 0;
+int overlayHeight = 0;
+
+// function prototypes
+void initD3D(HWND hWnd);    // sets up and initializes Direct3D
+void render_frame(void);    // renders a single frame
+void cleanD3D(void);    // closes Direct3D and releases memory
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);*/
